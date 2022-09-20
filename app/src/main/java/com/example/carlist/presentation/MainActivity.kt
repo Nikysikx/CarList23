0a0c57c0ba3d779d3316efb4532dd02fce0d8a38
Nicka <nickagordeeva@gmail.com>
2022-09-20 11:17:15 +0300

    presentation

--- app/src/main/java/com/example/carlist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/carlist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.carlist
+package com.example.carlist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.carlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
